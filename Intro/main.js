14eaf73b999802b981bf8ce5dfb5c50e1a03ad4b
Matheus-Maciel <mat_rod_mac@hotmail.com>
2019-04-29 15:53:21 -0300

    Correção de erros do código

---
+++ Intro/main.js
@@ -0,0 +1,17 @@
+var banners = [
+	"Espero que esteja funcionando",
+	"Está funcionando!!",
+	"Praise the sun \\o/"
+	];
+
+var bannerAtual = -1;
+
+function trocaBanner() {
+	bannerAtual = (bannerAtual + 1);
+	if (bannerAtual>2){
+		bannerAtual=0;
+	}
+	document.querySelector('h2#mensagem').textContent = banners[bannerAtual];
+}
+
+setInterval(trocaBanner, 1500);
