6f19053bc0951177841cf896bdb7e761e92be19a
Matheus-Maciel <mat_rod_mac@hotmail.com>
2019-04-29 11:20:07 -0300

    Script de troca de banner

---
+++ Intro/main.js
@@ -0,0 +1,17 @@
+var banners = [
+	"Espero que esteja funcionando",
+	"Está funcionando!!",
+	"Praise the sun \o/"
+	];
+
+var bannerAtual = 0;
+
+functon trocaBanner() {
+	bannerAtual = (bannerAtual + 1);
+	if (bannerAtual>2){
+		bannerAtual=0;
+	};
+	document.querySelector('h2#mensagem').textContent = banners[bannerAtual];
+}
+
+setInterval(trocaBanner, 2000);
